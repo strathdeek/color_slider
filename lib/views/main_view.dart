382c22740b1650bfbc32beb0358b60f9a3c27e90
Strathdeek <kstrath818@gmail.com>
2022-01-23 18:14:30 +0100

    implement color tiles

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,15 +1,122 @@
+import 'package:color_slider/utils/color_utils.dart';
 import 'package:color_slider/views/tile.dart';
 import 'package:flutter/material.dart';
 
-class MainView extends StatelessWidget {
+class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
 
   @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  final int columns = 10;
+  final int rows = 10;
+  final tileSize = 50.0;
+  final spacing = 20.0;
+
+  List<List<Color>> grid = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var col = 0; col < columns; col++) {
+      grid.add([]);
+      for (var row = 0; row < rows; row++) {
+        grid[col].add(randomColor());
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Tile(
-        color: Colors.red,
+    return Center(
+      child: Stack(
+        children: _getTiles(),
       ),
     );
   }
+
+  List<Widget> _getTiles() {
+    final newTiles = <Positioned>[];
+    for (var col = 0; col < columns; col++) {
+      for (var row = 0; row < rows; row++) {
+        newTiles.add(
+          Positioned(
+            top: _getTopAnchorForColumn(col),
+            left: _getleftAnchorForRow(row),
+            key: ValueKey('$col,$row'),
+            child: Tile(
+              height: tileSize,
+              width: tileSize,
+              padding: spacing,
+              color: grid[col][row],
+              getColorForDirection: (direction) {
+                try {
+                  switch (direction) {
+                    case Direction.up:
+                      return grid[col - 1][row];
+                    case Direction.down:
+                      return grid[col + 1][row];
+                    case Direction.left:
+                      return grid[col][row - 1];
+                    case Direction.right:
+                      return grid[col][row + 1];
+                  }
+                } catch (_) {
+                  return Colors.transparent;
+                }
+              },
+              onNewSlideDirection: (direction) {
+                try {
+                  String? key;
+
+                  switch (direction) {
+                    case Direction.up:
+                      key = '${col - 1},$row';
+                      break;
+                    case Direction.down:
+                      key = '${col + 1},$row';
+                      break;
+                    case Direction.left:
+                      key = '$col,${row - 1}';
+                      break;
+                    case Direction.right:
+                      key = '$col,${row + 1}';
+                      break;
+                    case null:
+                      for (final element in newTiles) {
+                        (element.child as Tile).setSlideDirection(direction);
+                      }
+                      break;
+                  }
+                  if (key?.isNotEmpty ?? false) {
+                    final slideTargetKey = ValueKey(key!);
+                    final slideSenderKey = ValueKey('$col,$row');
+
+                    for (final element in newTiles) {
+                      if (element.key == slideTargetKey) {
+                        (element.child as Tile)
+                            .setSlideDirection(direction?.opposite());
+                      } else if (element.key != slideSenderKey) {
+                        (element.child as Tile).setSlideDirection(null);
+                      }
+                    }
+                  }
+                } catch (_) {}
+              },
+            ),
+          ),
+        );
+      }
+    }
+    return newTiles;
+  }
+
+  // double _getTopAnchorForColumn(int col) => ((col + 1) * 12.5) + (col * 62.5);
+  // double _getleftAnchorForRow(int row) => (row + 1) * 12.5 + (row * 62.5);
+  double _getTopAnchorForColumn(int col) =>
+      spacing + (col * (tileSize + spacing));
+  double _getleftAnchorForRow(int row) =>
+      spacing + (row * (tileSize + spacing));
 }
--- lib/views/tile.dart
+++ lib/views/tile.dart
@@ -1,80 +1,241 @@
 import 'package:flutter/material.dart';
 
-enum Direction { up, down, left, right }
-
 class Tile extends StatefulWidget {
-  const Tile({Key? key, required this.color}) : super(key: key);
+  Tile(
+      {Key? key,
+      required this.color,
+      this.height = 50,
+      this.width = 50,
+      this.padding = 25,
+      required this.getColorForDirection,
+      required this.onNewSlideDirection})
+      : super(key: key);
 
-  @override
-  State<Tile> createState() => _TileState();
-
-  final Color color;
-}
-
-class _TileState extends State<Tile> {
-  static double shadowOffset = 10;
-  Direction? slideDirection;
-  final _offsetLeft = Offset(-shadowOffset, 0);
-  final _offsetRight = Offset(shadowOffset, 0);
-  final _offsetUp = Offset(0, shadowOffset);
-  final _offsetDown = Offset(0, -shadowOffset);
-
-  void _onPanUpdate(DragUpdateDetails details) {
-    final x = details.localPosition.dx - 25;
-    final y = details.localPosition.dy - 25;
-
-    if (x.abs() < 25 && y.abs() < 25) {
-      setState(() {
-        slideDirection = null;
-      });
-    } else {
-      setState(() {
-        if (x.abs() > y.abs()) {
-          slideDirection = x > 0 ? Direction.right : Direction.left;
-        } else {
-          slideDirection = y > 0 ? Direction.up : Direction.down;
-        }
-      });
-    }
+  void setSlideDirection(Direction? direction) {
+    _tileState.setSlideDirection(direction);
   }
 
   @override
+  State<Tile> createState() => _tileState;
+
+  final _TileState _tileState = _TileState();
+  final Color color;
+  final double height;
+  final double width;
+  final double padding;
+  final Color Function(Direction) getColorForDirection;
+  final Function(Direction?) onNewSlideDirection;
+}
+
+class _TileState extends State<Tile> {
+  Direction? slideDirection;
+  static double cornerRadius = 10;
+  static int animationTimeInMS = 150;
+  static Curve animationCurve = Curves.ease;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: _onPanUpdate,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 150),
-        curve: Curves.ease,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: widget.color,
-              offset: _getShadowOffset(),
+    return SizedBox(
+      height: widget.height + widget.padding * 2,
+      width: widget.width + widget.padding * 2,
+      child: Stack(
+        children: [
+          AnimatedPositioned(
+            height: _getShadowHeight(),
+            width: _getShadowWidth(),
+            duration: Duration(milliseconds: animationTimeInMS),
+            top: _getShadowTopAnchor(),
+            left: _getShadowLeftAnchor(),
+            curve: animationCurve,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: _getShadowGradient(),
+              ),
             ),
-          ],
-          color: widget.color,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(10),
           ),
-        ),
-        height: 50,
-        width: 50,
+          Center(
+            child: GestureDetector(
+              onPanUpdate: _onPanUpdate,
+              child: AnimatedContainer(
+                curve: animationCurve,
+                duration: Duration(milliseconds: animationTimeInMS),
+                height: widget.height,
+                width: widget.width,
+                decoration: BoxDecoration(
+                  color: widget.color,
+                  borderRadius: _getBorderRadius(),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Offset _getShadowOffset() {
+  void setSlideDirection(Direction? direction) {
+    setState(() {
+      slideDirection = direction;
+    });
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    final x = details.localPosition.dx - 25;
+    final y = details.localPosition.dy - 25;
+    Direction? newDirection;
+
+    if (x.abs() < 25 && y.abs() < 25) {
+      newDirection = null;
+    } else {
+      if (x.abs() > y.abs()) {
+        newDirection = x > 0 ? Direction.right : Direction.left;
+      } else {
+        newDirection = y > 0 ? Direction.down : Direction.up;
+      }
+    }
+
+    if (newDirection != slideDirection) {
+      setState(() {
+        slideDirection = newDirection;
+      });
+      widget.onNewSlideDirection(newDirection);
+    }
+  }
+
+  BorderRadius _getBorderRadius() {
     switch (slideDirection) {
       case Direction.left:
-        return _offsetLeft;
+        return BorderRadius.horizontal(
+          right: Radius.circular(cornerRadius),
+        );
+
       case Direction.right:
-        return _offsetRight;
+        return BorderRadius.horizontal(
+          left: Radius.circular(cornerRadius),
+        );
       case Direction.down:
-        return _offsetDown;
+        return BorderRadius.vertical(
+          top: Radius.circular(cornerRadius),
+        );
       case Direction.up:
-        return _offsetUp;
+        return BorderRadius.vertical(
+          bottom: Radius.circular(cornerRadius),
+        );
       case null:
-        return Offset.zero;
+        return BorderRadius.all(
+          Radius.circular(cornerRadius),
+        );
+    }
+  }
+
+  double _getShadowLeftAnchor() {
+    switch (slideDirection) {
+      case Direction.left:
+        return 0;
+      case Direction.right:
+        return widget.width + widget.padding;
+      case Direction.down:
+      case Direction.up:
+      case null:
+        return widget.padding;
+    }
+  }
+
+  double _getShadowTopAnchor() {
+    switch (slideDirection) {
+      case Direction.down:
+        return widget.height + widget.padding;
+      case Direction.up:
+        return 0;
+      case Direction.left:
+      case Direction.right:
+      case null:
+        return widget.padding;
+    }
+  }
+
+  double _getShadowWidth() {
+    switch (slideDirection) {
+      case Direction.left:
+      case Direction.right:
+        return widget.padding;
+      case Direction.down:
+      case Direction.up:
+      case null:
+        return widget.width;
+    }
+  }
+
+  double _getShadowHeight() {
+    switch (slideDirection) {
+      case Direction.down:
+      case Direction.up:
+        return widget.padding;
+      case Direction.left:
+      case Direction.right:
+      case null:
+        return widget.height;
+    }
+  }
+
+  LinearGradient _getShadowGradient() {
+    switch (slideDirection) {
+      case Direction.down:
+        return LinearGradient(
+          colors: [
+            widget.color,
+            widget.getColorForDirection(slideDirection!),
+          ],
+          end: Alignment.bottomCenter,
+          begin: Alignment.topCenter,
+        );
+      case Direction.up:
+        return LinearGradient(
+          colors: [
+            widget.color,
+            widget.getColorForDirection(slideDirection!),
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        );
+      case Direction.left:
+        return LinearGradient(
+          colors: [
+            widget.color,
+            widget.getColorForDirection(slideDirection!),
+          ],
+          begin: Alignment.centerRight,
+          end: Alignment.centerLeft,
+        );
+      case Direction.right:
+        return LinearGradient(
+          colors: [
+            widget.color,
+            widget.getColorForDirection(slideDirection!),
+          ],
+        );
+      case null:
+        return const LinearGradient(
+          colors: [Colors.transparent, Colors.transparent],
+        );
+    }
+  }
+}
+
+enum Direction { up, down, left, right }
+
+extension Opposites on Direction {
+  Direction opposite() {
+    switch (this) {
+      case Direction.down:
+        return Direction.up;
+
+      case Direction.up:
+        return Direction.down;
+      case Direction.left:
+        return Direction.right;
+      case Direction.right:
+        return Direction.left;
     }
   }
 }
